68167e341ae19c9a412672cd176f92ae03b5571a
Victor Yuan <victor.2wy@gmail.com>
2026-07-07 14:46:39 -0700

    generate

---
+++ posts/2026-06-11-large-quarto-websites/generate-quarto-render-image.R
@@ -0,0 +1,11 @@
+# To generate an image assocaited with blog post
+# x-axis is number of pages in a quarto project
+# y-axis is render time in seconds
+
+# STEP 1: generate a temporary quarto project with X number of pages
+
+generate_quarto_project <- function(dir = fs::path_temp(), n_page = 3) {}
+
+# STEP 2: render and record time
+
+# STEP 3: generate figure
